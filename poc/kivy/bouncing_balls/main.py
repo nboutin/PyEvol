df3890893a82b5dc6122d2caf91f7eb5d3c714aa
nboutin <boutwork@gmail.com>
2019-07-13 17:40:12 +0200

    add balls on_touch_down

--- poc/kivy/bouncing_balls/main.py
+++ poc/kivy/bouncing_balls/main.py
@@ -5,10 +5,18 @@ Created on Jul 13, 2019
 '''
 
 from kivy.app import App
-from kivy.uix.widget import Widget
 from kivy.clock import Clock
+from kivy.uix.widget import Widget
+from kivy.graphics import Color, Ellipse
 
 class BouncingBallsWidget(Widget):
+    
+    def on_touch_down(self, touch):
+        with self.canvas:
+            Color(1, 1, 0)
+            d = 30.
+            Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
+    
     def update(self, dt):
         pass
 
---
+++ poc/kivy/bouncing_balls/readme.md
@@ -0,0 +1,15 @@
+# Bouncing Balls
+
+This is a demo application using Kivy.
+
+Ball can be added to the widget. A ball is:
+
+* created with random size
+* random velocity.
+* bouncing into each other and walls.
+
+Buttons are available:
+
+* Add one ball
+* Remove one ball
+* Reset all
