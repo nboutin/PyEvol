6efa48013c687f52828d0507d69e4dff51fb5526
nboutin <boutwork@gmail.com>
2019-07-13 17:49:40 +0200

    moving balls

--- poc/kivy/bouncing_balls/main.py
+++ poc/kivy/bouncing_balls/main.py
@@ -8,17 +8,24 @@ from kivy.app import App
 from kivy.clock import Clock
 from kivy.uix.widget import Widget
 from kivy.graphics import Color, Ellipse
+from kivy.vector import Vector
 
 class BouncingBallsWidget(Widget):
+    
+    def __init__(self, **kwargs):
+        Widget.__init__(self, **kwargs)
+        self.balls = list()
     
     def on_touch_down(self, touch):
         with self.canvas:
             Color(1, 1, 0)
             d = 30.
-            Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
+            b = Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
+            self.balls.append(b)
     
     def update(self, dt):
-        pass
+        for b in self.balls:
+            b.pos = Vector(4,0) + b.pos
 
 class BouncingBallsApp(App):
     def build(self):
