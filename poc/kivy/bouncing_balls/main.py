e5c5af6afcd792d1eaaf86fdef40a57853ad75a7
nboutin <boutwork@gmail.com>
2019-07-14 19:06:40 +0200

    create ball.py

--- poc/kivy/bouncing_balls/main.py
+++ poc/kivy/bouncing_balls/main.py
@@ -8,34 +8,11 @@ from kivy.app import App
 from kivy.clock import Clock
 from kivy.uix.widget import Widget
 from kivy.uix.label import Label
-from kivy.graphics import (Color, Ellipse, Rectangle)
+from kivy.graphics import (Color, Rectangle)
 from kivy.vector import Vector
 from kivy.properties import (NumericProperty, ReferenceListProperty, ObjectProperty)
-from random import uniform
 
-class Ball(Widget):
-    velocity_x = NumericProperty(0)
-    velocity_y = NumericProperty(0)
-    velocity = ReferenceListProperty(velocity_x, velocity_y)
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.velocity = Vector(uniform(-5,5), uniform(-5,5))
-        d = uniform(10,50)
-        self.size = (d,d)
-        x,y = kwargs['pos']
-        with self.canvas:
-            Color(0,0,0,1)
-            self.circle = Ellipse(pos=(x-d/2, y-d/2), size=self.size)
-        
-        self.bind(pos=self._update_circle, size=self._update_circle)
-        
-    def _update_circle(self, *args):
-        self.circle.pos = self.pos
-        self.circle.size = self.size
-        
-    def move(self):
-        self.pos = Vector(*self.velocity) + self.pos
+from ball import Ball
 
 class BouncingBallsWidget(Widget):
     
