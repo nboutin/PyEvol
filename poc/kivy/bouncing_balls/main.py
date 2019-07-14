b8d8f3298ddde5a0c9c819443430138c8f6986d9
nboutin <boutwork@gmail.com>
2019-07-14 14:14:13 +0200

    set background color

--- poc/kivy/bouncing_balls/main.py
+++ poc/kivy/bouncing_balls/main.py
@@ -7,25 +7,67 @@ Created on Jul 13, 2019
 from kivy.app import App
 from kivy.clock import Clock
 from kivy.uix.widget import Widget
-from kivy.graphics import Color, Ellipse
+from kivy.uix.label import Label
+from kivy.graphics import (Color, Ellipse, Rectangle)
 from kivy.vector import Vector
+from kivy.properties import (NumericProperty, ReferenceListProperty, ObjectProperty)
+
+class Ball(Widget):
+    velocity_x = NumericProperty(0)
+    velocity_y = NumericProperty(0)
+    velocity = ReferenceListProperty(velocity_x, velocity_y)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.center = (100,100)
+        self.r = 0
+
+    def move(self):
+        self.pos = Vector(*self.velocity) + self.pos
 
 class BouncingBallsWidget(Widget):
     
     def __init__(self, **kwargs):
-        Widget.__init__(self, **kwargs)
+        super().__init__(**kwargs)
         self.balls = list()
+        with self.canvas.before:
+            Color(0,1,0,1)
+            self.background = Rectangle(size=self.size, pos=self.pos)
+            
+        # listen to size and position changes
+        self.bind(pos=self._update_background, size=self._update_background)
+    
+    def _update_background(self, instance, value):
+        self.background.pos = instance.pos
+        self.background.size = instance.size
+
     
     def on_touch_down(self, touch):
-        with self.canvas:
-            Color(1, 1, 0)
-            d = 30.
-            b = Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
-            self.balls.append(b)
+        pass
+#         label = Label(text='0', pos=touch.pos)
+#         self.add_widget(label)
+
+#         with self.canvas:
+#             Color(1, 1, 0)
+#             b = Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
+#         d = 30.
+#         b = Ball(pos=(touch.x - d / 2, touch.y - d / 2))
+#         self.add_widget(b)
+#         self.balls.append(b)
     
     def update(self, dt):
-        for b in self.balls:
-            b.pos = Vector(4,0) + b.pos
+        pass
+#         for b in self.balls:
+# #             b.pos = Vector(4,0) + b.pos
+#             b.move()
+#             
+#             # bounce off top and bottom
+#             if (b.y < 0) or (b.top > self.height):
+#                 b.velocity_y *= -1
+# 
+#             # bounce off left and right
+#             if (b.x < 0) or (b.right > self.width):
+#                 b.velocity_x *= -1
 
 class BouncingBallsApp(App):
     def build(self):
