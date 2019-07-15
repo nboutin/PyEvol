248dea2e90f8bac9647060d82d54b0f1265afa1a
nboutin <boutwork@gmail.com>
2019-07-15 21:19:13 +0200

    add bouncing ball pymunk

---
+++ poc/kivy/pymunk/bouncing_balls/main.py
@@ -0,0 +1,39 @@
+'''
+Created on Jul 13, 2019
+
+@author: nboutin
+'''
+
+from kivy.app import App
+from kivy.clock import Clock
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+from functools import partial
+
+from world import World
+
+class BouncingBallsApp(App):
+    def build(self):
+        world = World()
+
+        population = Label(text='0')
+
+        btn_add10 = Button(text='+ 10 rects',
+                            on_press=partial(world.add_balls, 10))
+
+        layout = BoxLayout(size_hint=(1, None), height=50)
+        layout.add_widget(btn_add10)
+        layout.add_widget(population)
+
+        root = BoxLayout(orientation='vertical')
+        root.add_widget(world)
+        root.add_widget(layout)
+
+        Clock.schedule_interval(world.update, 1.0 / 60.0)
+        return root
+    pass
+
+
+if __name__ == '__main__':
+    BouncingBallsApp().run()
---
+++ poc/kivy/pymunk/bouncing_balls/readme.md
@@ -0,0 +1,18 @@
+# Bouncing Balls
+
+This is a demo application using Kivy.
+
+Ball can be added to the widget. A ball is:
+
+* created with random size
+* random velocity.
+* bouncing into walls.
+
+Buttons are available:
+
+* Add 10 ball
+
+# Todo
+- [ ] World is a donut (no border)
+- [ ] Ball collision into each other
+- [ ] Add physic engine
