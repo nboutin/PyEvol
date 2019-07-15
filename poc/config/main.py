cc8e5f1ac29942ae29e06c12fbf849118b2fc6ad
nboutin <boutwork@gmail.com>
2019-07-15 13:55:45 +0200

    test module config

--- poc/config/main.py
+++ poc/config/main.py
@@ -13,3 +13,16 @@ if __name__ == '__main__':
     cfg = Config(config_pathname)
     print (cfg)
     print (cfg.message)
+    print (cfg.get('badkey', "default value"))
+
+    for m in cfg.message_list:
+        print (m,' ',end='')
+    print()
+        
+    print (cfg.str1, '+', cfg.str2,'=',cfg.strconcat)
+    print (cfg.value1, '+', cfg.value2, '=', cfg.total)
+    
+#     Save is not support with Python3
+#     save_pathname = os.path.join(local_dir, 'save.cfg')
+#     save_file = open(save_pathname, 'w')
+#     cfg.save(save_file)
