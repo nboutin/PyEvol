75e4b566562162c82d0484148a3a1619d0d24901
nboutin <boutwork@gmail.com>
2019-07-31 21:04:49 +0200

    use kivy config to disable multitouch_sim

--- poc/kivy/scatter/main.py
+++ poc/kivy/scatter/main.py
@@ -4,9 +4,14 @@ Created on Jul 19, 2019
 @author: nboutin
 '''
 
+# disable multi-touch emulation
+from kivy.config import Config
+# Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
+# Config.set('input', 'mouse', 'mouse,disable_multitouch')
+# Config.set('input', 'mouse', 'disable_on_activity')
+Config.set('input', 'mouse', 'mouse,disable_multitouch,disable_on_activity')
 
 from kivy.app import App
-from kivy.uix.label import Label
 from kivy.uix.scatter import Scatter
 from kivy.graphics import (Color, Rectangle)
 from kivy.graphics.transformation import Matrix
@@ -17,60 +22,50 @@ class World(Scatter):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
+        self.do_rotation = False
         self.scale_min = .1
         self.scale_max = 10
         self.top = 500
 
-        with self.canvas:
-            Color(0, 1, .5, mode='hsv')
-            self.rectA = Rectangle(size=self.size)
-
-        self.bind(size=self._update_rectA)
-        
         with self.canvas.after:
-            Color(.1, 1, .5, .2, mode='hsv')
-            self.rectB = Rectangle(pos=self.pos, size=self.bbox[1])
-            
-        self.bind(pos=self._update_rectB, size=self._update_rectB)
+            Color(0, 1, 0)
+            self.rect = Rectangle(pos=self.pos, size=self.bbox[1])
 
-    def _update_rectA(self, instance, _):
-        self.rectA.size = self.size
+        self.bind(pos=self._update_rect, size=self._update_rect)
 
-    def _update_rectB(self, instance, _):
-        self.rectB.pos = self.pos
-        self.rectB.size = self.bbox[1]
-        
-    def on_transform_with_touch(self, touch):
-        print("on_transform_with_touch:", touch)
-        
-    def on_touch_move(self, touch):
-#         print ("on_touch_move:", touch)
-        self.center = touch.pos
-        
-    def on_touch_down(self, touch):
-        pass
-#         if touch.is_mouse_scrolling:
-#             if touch.button == 'scrolldown':
-#                 print("scrolldown")
-#                 print(self.transform)
-#                 mat = Matrix().scale(.9, .9, .9)
-#                 self.apply_transform(mat)
-#                 print(self.transform)
-#                 print(self.pos)
-#                 print(self.rect.pos)
-#                 print(self.to_local(1, 1, relative=True))
-#                 print(self.to_widget(1, 1, relative=True))
-#                 print(self.to_parent(1, 1, relative=True))
-#                 self.rect.pos = self.to_local()
-#             elif touch.button == 'scrollup':
-#                 print("scrollup")
-#                 print(self.transform)
-#                 mat = Matrix().scale(1.1, 1.1, 1.1)
-#                 self.apply_transform(mat)
+    def _update_rect(self, instance, _):
+        self.rect.pos = self.pos
+        self.rect.size = self.bbox[1]
 
 #     def on_transform_with_touch(self, touch):
-#         print("on_transform_with_touch")
+#         print("on_transform_with_touch:", touch)
 
+    def on_touch_move(self, touch):
+        print("on_touch_move:", touch.profile, touch)
+        return super().on_touch_move(touch)
+
+    def on_touch_down(self, touch):
+        print("on_touch_down:", touch.profile, touch)
+        return super().on_touch_down(touch)
+
+    def on_touch_up(self, touch):
+        if self.collide_point(*touch.pos):
+            print("on_touch_up:", touch.profile, touch)
+
+            if touch.is_mouse_scrolling:
+                print ("is_mouse_scrolling")
+                if touch.button == 'scrolldown':
+                    print("scrolldown")
+                    mat = Matrix().scale(.9, .9, .9)
+                    self.apply_transform(mat, anchor=touch.pos)
+                    return True
+                elif touch.button == 'scrollup':
+                    print("scrollup")
+                    mat = Matrix().scale(1.1, 1.1, 1.1)
+                    self.apply_transform(mat, anchor=touch.pos)
+                    return True
+        
+        return super().on_touch_up(touch)
 
 class ScatterApp(App):
     def build(self):
