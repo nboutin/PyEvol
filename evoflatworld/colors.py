adecf43ec56f7568382afa7b7a6b475db5f9e088
nboutin <boutwork@gmail.com>
2019-08-06 10:12:21 +0200

    add colors module

---
+++ evoflatworld/colors.py
@@ -0,0 +1,10 @@
+'''
+Created on Aug 6, 2019
+
+@author: nboutin
+'''
+from kivy.utils import rgba
+
+black = rgba(0, 0, 0)
+white = rgba(1, 1, 1)
+green = rgba(168, 213, 186)  # pantone 2246c
--- evoflatworld/game_system/world_render_widget_strategy.py
+++ evoflatworld/game_system/world_render_widget_strategy.py
@@ -4,19 +4,17 @@ Created on Aug 2, 2019
 @author: nboutin
 '''
 from kivy.uix.widget import Widget
-# from kivy.uix.relativelayout import RelativeLayout
 from kivy.graphics import (Color, Rectangle)
 
 from evoflatworld.game_system.i_render_strategy import IRenderStrategy
-
+import colors
 
 class WorldRenderWidgetStrategy(IRenderStrategy, Widget):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         with self.canvas:
-#             Color(0, 1, 0, 1)
-            Color(0, 0, 0)
+            Color(*colors.green)
             self.__rect = Rectangle(pos=self.pos, size=self.size)
 
 #         self.bind(pos=self._update_rect, size=self._update_rect)
